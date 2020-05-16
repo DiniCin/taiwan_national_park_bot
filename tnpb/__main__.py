8e99bd9bc2f0740b5fc496c76b8db2925ac7f613
Dini <dini7777777@gmail.com>
2020-05-16 11:05:47 +0800

    Schedule it 1 min earlier.

--- tnpb/__main__.py
+++ tnpb/__main__.py
@@ -1,9 +1,10 @@
-import os
+import os, time
 from tnpb.bot import TNPBot
 
 
 
 def main():
+    time.sleep(10)
     tnp_bot = TNPBot()
 
     resp = tnp_bot.get_query_result(id=os.getenv("ID"), email=os.getenv("EMAIL"))

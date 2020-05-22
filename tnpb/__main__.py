c94bb69c194ee4d7a23ffe79d1a86bf9e61cba3c
chingting.wu_Cin <chingting.wu@cinnamon.is>
2020-05-22 08:11:48 +0800

    Update __main__.py

--- tnpb/__main__.py
+++ tnpb/__main__.py
@@ -4,7 +4,7 @@ from tnpb.bot import TNPBot
 
 
 def main():
-    time.sleep(3)
+    time.sleep(10)
     tnp_bot = TNPBot()
 
     resp = tnp_bot.get_query_result(id=os.getenv("ID"), email=os.getenv("EMAIL"))

22d11e793b35d809505761000d9245c96202849f
Dini <dini7777777@gmail.com>
2020-06-14 23:47:15 +0800

    Add exception handeling retry.

--- tnpb/__main__.py
+++ tnpb/__main__.py
@@ -5,10 +5,20 @@ from tnpb.bot import TNPBot
 
 def main():
     time.sleep(10)
-    tnp_bot = TNPBot()
-
-    resp = tnp_bot.get_query_result(id=os.getenv("ID"), email=os.getenv("EMAIL"))
-    tnp_bot.send_application(resp)
+    serial_num = False
+    
+    for i in range(10):
+        try:
+            tnp_bot = TNPBot()
+            resp = tnp_bot.get_query_result(id=os.getenv("ID"),
+                                            email=os.getenv("EMAIL"))
+            serial_num = tnp_bot.send_application(resp)
+            
+        except AttributeError:
+            print("Failed in the {} th session".format(i+1))
+            pass
+        if serial_num:
+            break
     
 if __name__ == "__main__":
     main()
